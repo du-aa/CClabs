6ba409d9a1eb7f1e13f95550472f992c70b24827
du-aa <92159072+du-aa@users.noreply.github.com>
2025-01-16 18:26:02 +0500

    Add files via upload

---
+++ lab11/lab11/Program.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TinyCParser
+{
+    class Program
+    {
+        // Symbol Table to store variable types
+        static Dictionary<string, string> symbolTable = new Dictionary<string, string>();
+
+        static void Main(string[] args)
+        {
+            // 1. Define the SLR(1) Parsing Table (as a Dictionary) - same as before
+
+            var parsingTable = new Dictionary<(string, string), string>()
+            {
+             { ("I0", "int"), "s5" },    { ("I0", "float"), "s6" },  {("I0", "program"), "1"},
+                {("I0", "declarations"), "2" }, {("I0", "declaration"), "3"}, {("I0", "type"), "4"},
+              { ("I1", "$"), "acc" },
+                {("I2", "int"), "s5"},   { ("I2", "float"), "s6" },  { ("I2", "id"), "s11" },   {("I2", "statements"), "7"}, {("I2","statement"),"8"},
+                 {("I3", "int"), "s5"}, {("I3", "float"), "s6"}, {("I3", "declarations"), "9"},{("I3","declaration"),"3"}, {("I3", "type"),"4"},
+                {("I4", "id"),"s10"},
+                 {("I5", null), "r4"},{("I6", null), "r4"},
+                 {("I7", "$"), "r1"},
+                {("I8","id"), "s11"}, {("I8", "$"), "r5"}, {("I8", "statement"), "8"},
+                 {("I9","$"),"r2"}, {("I9","id"),"r2"}, {("I9","int"),"r2"},{("I9","float"),"r2"},
+                 {("I10", ";"), "s17"},
+                 {("I11", "="), "s12"},
+                {("I12", "id"), "s14"}, { ("I12", "int_const"), "s15" }, {("I12", "float_const"), "s16"}, {("I12","expr"),"13"},
+                {("I13", ";"), "s18"},
+                 {("I14", ";"), "r7"},
+                 {("I15", ";"), "r7"},
+                 {("I16", ";"), "r7"},
+                 {("I17", "$"), "r3"},
+                 {("I17", "id"), "r3"},
+                 {("I17", "int"), "r3"},
+                {("I17", "float"), "r3"},
+                 {("I18", "$"), "r6"}, {("I18", "id"),"r6"},
+            };
+            // 2. Define the grammar rules
+            var grammarRules = new Dictionary<int, (string, string[])>()
+            {
+                { 1, ("program", new[] { "declarations", "statements" }) },
+                { 2, ("declarations", new[] { "declaration", "declarations" }) },
+                { 3, ("declaration", new[] { "type", "id", ";" }) },
+                 { 4, ("type", new[] { "int" }) },
+                 { 5, ("type", new[] { "float" }) },
+                { 6, ("statements", new[] { "statement", "statements" }) },
+                { 7, ("statement", new[] { "id", "=", "expr", ";" }) },
+                 { 8, ("expr", new[] { "id" }) },
+                  { 9, ("expr", new[] { "int_const" }) },
+                 { 10, ("expr", new[] { "float_const" }) },
+            };
+
+            Console.WriteLine("Enter a TINY C program (tokens separated by spaces):");
+            string input = Console.ReadLine() + " $";
+            Queue<string> tokenQueue = new Queue<string>(input.Split(" ").Where(token => !string.IsNullOrWhiteSpace(token)));
+
+            var stack = new Stack<string>();
+            stack.Push("I0");
+
+            while (tokenQueue.Count > 0)
+            {
+                string currentToken = tokenQueue.Peek();
+                string currentState = stack.Peek();
+
+                Console.WriteLine($"Current state: {currentState}, Token: {currentToken}");
+
+                if (parsingTable.TryGetValue((currentState, currentToken), out string action))
+                {
+                    if (action.StartsWith("s")) // shift action
+                    {
+                        stack.Push(currentToken);
+                        stack.Push(action.Substring(1));
+                        tokenQueue.Dequeue();
+                    }
+                    else if (action.StartsWith("r"))//reduce action
+                    {
+                        int ruleNumber = int.Parse(action.Substring(1));
+                        var rule = grammarRules[ruleNumber];
+
+                        // Perform semantic actions during reduction
+                        PerformSemanticAnalysis(rule, stack, parsingTable, tokenQueue, currentToken);
+
+                        for (int i = 0; i < rule.Item2.Length; i++)
+                        {
+                            stack.Pop(); // remove the states added by the rule
+                            stack.Pop(); //remove the grammar symbols from stack added by the rules
+                        }
+
+                        string nextState = stack.Peek();
+
+                        if (parsingTable.TryGetValue((nextState, rule.Item1), out string gotoState))
+                        {
+                            stack.Push(rule.Item1); // Push left hand non-terminal to stack
+                            stack.Push(gotoState);  //push state to stack
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error: invalid syntax at " + currentToken);
+                            return;
+                        }
+                    }
+                    else if (action == "acc")
+                    {
+                        Console.WriteLine("Parsing and Semantic Analysis successful!");
+                        return;
+                    }
+
+                }
+                else if (parsingTable.TryGetValue((currentState, null), out string nullAction))
+                {
+                    if (nullAction.StartsWith("r")) // handles reduce rules on non terminals.
+                    {
+                        int ruleNumber = int.Parse(nullAction.Substring(1));
+                        var rule = grammarRules[ruleNumber];
+
+                        // Perform semantic actions during reduction
+                        PerformSemanticAnalysis(rule, stack, parsingTable, tokenQueue, currentToken);
+
+                        for (int i = 0; i < rule.Item2.Length; i++)
+                        {
+                            stack.Pop(); // remove the states added by the rule
+                            stack.Pop(); //remove the grammar symbols from stack added by the rules
+                        }
+
+                        string nextState = stack.Peek();
+
+                        if (parsingTable.TryGetValue((nextState, rule.Item1), out string gotoState))
+                        {
+                            stack.Push(rule.Item1); // Push left hand non-terminal to stack
+                            stack.Push(gotoState);  //push state to stack
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error: invalid syntax at " + currentToken);
+                            return;
+                        }
+                    }
+
+                    else
+                    {
+                        Console.WriteLine($"Error: Invalid input token '{currentToken}' in state '{currentState}'.");
+                        return;
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine($"Error: Invalid input token '{currentToken}' in state '{currentState}'.");
+                    return;
+                }
+            }
+            Console.WriteLine("Error: Input not fully processed.");
+
+        }
+
+        static void PerformSemanticAnalysis((string, string[]) rule, Stack<string> stack, Dictionary<(string, string), string> parsingTable, Queue<string> tokenQueue, string currentToken)
+        {
+            switch (rule.Item1)
+            {
+                case "declaration":
+                    // Process declaration and add type of the variable to symbol table.
+                    string type = stack.ElementAt(1);
+                    string id = stack.ElementAt(2);
+                    if (symbolTable.ContainsKey(id))
+                    {
+                        Console.WriteLine($"Error: variable '{id}' already declared.");
+                        Environment.Exit(1);
+                    }
+                    symbolTable[id] = type;
+                    break;
+                case "statement":
+
+                    //Perform assignment type check
+
+                    string idVariable = stack.ElementAt(3);
+                    string exprType = GetExpressionType(stack.ElementAt(1), parsingTable, tokenQueue);
+
+                    if (symbolTable.ContainsKey(idVariable))
+                    {
+                        string variableType = symbolTable[idVariable];
+
+                        if (!IsTypeCompatible(variableType, exprType))
+                        {
+                            Console.WriteLine($"Error: Type mismatch in assignment.  Variable '{idVariable}' is of type '{variableType}' but expression is of type '{exprType}'.");
+                            Environment.Exit(1);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Error: undeclared variable '{idVariable}'.");
+                        Environment.Exit(1);
+                    }
+                    break;
+            }
+        }
+
+        // Get type of expression
+        static string GetExpressionType(string expressionToken, Dictionary<(string, string), string> parsingTable, Queue<string> tokenQueue)
+        {
+            if (expressionToken == "id" || parsingTable.ContainsKey((expressionToken, "id")))
+            {
+                if (symbolTable.TryGetValue(tokenQueue.ElementAt(0), out string type))
+                {
+                    return type;
+                }
+                else
+                {
+                    Console.WriteLine($"Error: undeclared identifier '{tokenQueue.ElementAt(0)}'.");
+                    Environment.Exit(1);
+                }
+            }
+            else if (expressionToken == "int_const" || parsingTable.ContainsKey((expressionToken, "int_const")))
+            {
+                return "int";
+            }
+            else if (expressionToken == "float_const" || parsingTable.ContainsKey((expressionToken, "float_const")))
+            {
+                return "float";
+            }
+            return "unknown";
+        }
+        // Check compatibility
+        static bool IsTypeCompatible(string varType, string exprType)
+        {
+            if (varType == exprType)
+            {
+                return true;
+            }
+            if (varType == "float" && exprType == "int")
+            {
+                return true; // int can be implicitly converted to float
+            }
+
+            return false;
+        }
+    }
+}
---
+++ lab12/lab12/Program.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace LexicalAnalyzerConsole
+{
+    class Program
+    {
+        static List<List<String>> SymbolTable = new List<List<String>>();
+        static ArrayList LineNumber;
+        static ArrayList Variables;
+        static ArrayList KeyWords;
+        static ArrayList Constants;
+        static Regex variable_Reg;
+        static Regex constants_Reg;
+        static Regex operators_Reg;
+        static Regex special_Reg;
+        static int lexemes_per_line;
+        static int ST_index;
+
+        static void Main(string[] args)
+        {
+            // Initialization (similar to the Form's constructor)
+            string[] k_ = { "int", "float", "begin", "end", "print", "if", "else", "String", "char", "double" };
+            ArrayList key = new ArrayList(k_);
+            LineNumber = new ArrayList();
+            Variables = new ArrayList();
+            KeyWords = new ArrayList();
+            Constants = new ArrayList();
+            variable_Reg = new Regex(@"^[A-Za-z|_][A-Za-z|0-9]*$");
+            constants_Reg = new Regex(@"^[0-9]+([.][0-9]+)?([e]([+|-])?[0-9]+)?$");
+            operators_Reg = new Regex(@"^[-*+/><&&||=]$");
+            special_Reg = new Regex(@"^[.,'\[\]{}();:?]$");
+
+
+            Console.WriteLine("Enter your Tiny C program (end with an empty line):");
+            string userInput = "";
+            string line;
+            while ((line = Console.ReadLine()) != null && line != "")
+            {
+                userInput += line + "\n";
+            }
+            AnalyzeInput(userInput);
+            DisplaySymbolTable();
+            Console.ReadKey();
+        }
+        static void AnalyzeInput(string userInput)
+        {
+            List<String> keywordList = new List<String>
+          {
+             "int","float","while","main","if","else","new","String","char","double"
+           };
+            int row = 1;
+            int count = 1;
+            int line_num = 0;
+            string[,] SymbolTableArray = new string[20, 6];
+            List<String> varListinSymbolTable = new List<String>();
+
+            ArrayList finalArray = new ArrayList();
+            ArrayList finalArrayc = new ArrayList();
+            ArrayList tempArray = new ArrayList();
+            char[] charinput = userInput.ToCharArray();
+
+            for (int itr = 0; itr < charinput.Length; itr++)
+            {
+                Match Match_Variable = variable_Reg.Match(charinput[itr] + "");
+                Match Match_Constant = constants_Reg.Match(charinput[itr] + "");
+                Match Match_Operator = operators_Reg.Match(charinput[itr] + "");
+                Match Match_Special = special_Reg.Match(charinput[itr] + "");
+                if (Match_Variable.Success || Match_Constant.Success || Match_Operator.Success || Match_Special.Success || charinput[itr].Equals(' '))
+                {
+                    tempArray.Add(charinput[itr]);
+                }
+                if (charinput[itr].Equals('\n'))
+                {
+                    if (tempArray.Count != 0)
+                    {
+                        string fin = "";
+                        for (int j = 0; j < tempArray.Count; j++)
+                        {
+                            fin += tempArray[j];
+                        }
+                        finalArray.Add(fin);
+                        tempArray.Clear();
+                    }
+                }
+            }
+            if (tempArray.Count != 0)
+            {
+                string fin = "";
+                for (int j = 0; j < tempArray.Count; j++)
+                {
+                    fin += tempArray[j];
+                }
+                finalArray.Add(fin);
+                tempArray.Clear();
+            }
+            Console.WriteLine("Tokens:");
+
+            SymbolTable.Clear();
+
+            for (int i = 0; i < finalArray.Count; i++)
+            {
+                string line = finalArray[i].ToString();
+                char[] lineChar = line.ToCharArray();
+                line_num++;
+
+                for (int itr = 0; itr < lineChar.Length; itr++)
+                {
+                    Match Match_Variable = variable_Reg.Match(lineChar[itr] + "");
+                    Match Match_Constant = constants_Reg.Match(lineChar[itr] + "");
+                    Match Match_Operator = operators_Reg.Match(lineChar[itr] + "");
+                    Match Match_Special = special_Reg.Match(lineChar[itr] + "");
+                    if (Match_Variable.Success || Match_Constant.Success)
+                    {
+                        tempArray.Add(lineChar[itr]);
+                    }
+                    if (lineChar[itr].Equals(' '))
+                    {
+                        if (tempArray.Count != 0)
+                        {
+                            string fin = "";
+                            for (int j = 0; j < tempArray.Count; j++)
+                            {
+                                fin += tempArray[j];
+                            }
+                            finalArrayc.Add(fin);
+                            tempArray.Clear();
+                        }
+                    }
+                    if (Match_Operator.Success || Match_Special.Success)
+                    {
+                        if (tempArray.Count != 0)
+                        {
+                            string fin = "";
+                            for (int j = 0; j < tempArray.Count; j++)
+                            {
+                                fin += tempArray[j];
+                            }
+                            finalArrayc.Add(fin);
+                            tempArray.Clear();
+                        }
+                        finalArrayc.Add(lineChar[itr]);
+                    }
+                }
+                if (tempArray.Count != 0)
+                {
+                    string fina = "";
+                    for (int k = 0; k < tempArray.Count; k++)
+                    {
+                        fina += tempArray[k];
+                    }
+                    finalArrayc.Add(fina);
+                    tempArray.Clear();
+                }
+
+                for (int x = 0; x < finalArrayc.Count; x++)
+                {
+                    Match operators = operators_Reg.Match(finalArrayc[x].ToString());
+                    Match variables = variable_Reg.Match(finalArrayc[x].ToString());
+                    Match digits = constants_Reg.Match(finalArrayc[x].ToString());
+                    Match punctuations = special_Reg.Match(finalArrayc[x].ToString());
+                    if (operators.Success)
+                    {
+                        Console.Write("< op, " + finalArrayc[x].ToString() + "> ");
+                    }
+                    else if (digits.Success)
+                    {
+                        Console.Write("< digit, " + finalArrayc[x].ToString() + "> ");
+                    }
+                    else if (punctuations.Success)
+                    {
+                        Console.Write("< punc, " + finalArrayc[x].ToString() + "> ");
+                    }
+                    else if (variables.Success)
+                    {
+                        if (!keywordList.Contains(finalArrayc[x].ToString()))
+                        {
+                            Regex reg1 = new Regex(@"^(int|float|double)\s([A-Za-z|_][A-Za-z|0-9]{0,10})\s(=)\s([0-9]+([.][0-9]+)?([e][+|-]?[0-9]+)?)\s(;)$");
+                            Match category1 = reg1.Match(line);
+                            Regex reg2 = new Regex(@"^(String|char)\s([A-Za-z|_][A-Za-z|0-9]{0,10})\s(=)\s[']\s([A-Za-z|_][A-Za-z|0-9]{0,30})\s[']\s(;)$");
+                            Match category2 = reg2.Match(line);
+
+                            if (category1.Success)
+                            {
+                                SymbolTableArray[row, 1] = row.ToString();
+                                SymbolTableArray[row, 2] = finalArrayc[x].ToString();
+                                SymbolTableArray[row, 3] = finalArrayc[x - 1].ToString();
+                                SymbolTableArray[row, 4] = finalArrayc[x + 2].ToString();
+                                SymbolTableArray[row, 5] = line_num.ToString();
+                                Console.Write("<var" + count + ", " + row + "> ");
+
+                                row++;
+                                count++;
+                            }
+
+                            else if (category2.Success)
+                            {
+                                if (!(finalArrayc[x - 1].ToString().Equals("'") && finalArrayc[x + 1].ToString().Equals("'")))
+                                {
+                                    SymbolTableArray[row, 1] = row.ToString();
+                                    SymbolTableArray[row, 2] = finalArrayc[x].ToString();
+                                    SymbolTableArray[row, 3] = finalArrayc[x - 1].ToString();
+                                    SymbolTableArray[row, 4] = finalArrayc[x + 3].ToString();
+                                    SymbolTableArray[row, 5] = line_num.ToString();
+                                    Console.Write("<var" + count + ", " + row + "> ");
+
+                                    row++;
+                                    count++;
+                                }
+                                else
+                                {
+                                    Console.Write("<String" + count + ", " + finalArrayc[x].ToString() + "> ");
+                                }
+
+                            }
+                            else
+                            {
+                                string ind = "Default";
+                                string ty = "Default";
+                                string val = "Default";
+                                string lin = "Default";
+                                for (int r = 1; r <= SymbolTableArray.GetLength(0); r++)
+                                {
+                                    if (SymbolTableArray[r, 2] != null && SymbolTableArray[r, 2].Equals(finalArrayc[x].ToString()))
+                                    {
+                                        ind = SymbolTableArray[r, 1];
+                                        ty = SymbolTableArray[r, 3];
+                                        val = SymbolTableArray[r, 4];
+                                        lin = SymbolTableArray[r, 5];
+                                        Console.Write("<var" + ind + ", " + ind + "> ");
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Console.Write("<keyword, " + finalArrayc[x].ToString() + "> ");
+                        }
+                    }
+
+                }
+                Console.WriteLine();
+                finalArrayc.Clear();
+            }
+            SymbolTable.Clear();
+            for (int r = 1; r < SymbolTableArray.GetLength(0); r++)
+            {
+                if (SymbolTableArray[r, 1] != null)
+                {
+                    List<string> entry = new List<string>();
+                    entry.Add(SymbolTableArray[r, 1]);
+                    entry.Add(SymbolTableArray[r, 2]);
+                    entry.Add(SymbolTableArray[r, 3]);
+                    entry.Add(SymbolTableArray[r, 4]);
+                    entry.Add(SymbolTableArray[r, 5]);
+                    SymbolTable.Add(entry);
+                }
+            }
+        }
+
+        static void DisplaySymbolTable()
+        {
+            Console.WriteLine("\nSymbol Table:");
+            Console.WriteLine("Index\tVariable Name\tType\tValue\tLine#");
+            foreach (var entry in SymbolTable)
+            {
+                foreach (var item in entry)
+                {
+                    Console.Write(item + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
+
