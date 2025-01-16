2ff096c9b8b7fecfccdca937ddfc7f9192737aa4
du-aa <92159072+du-aa@users.noreply.github.com>
2025-01-16 18:27:41 +0500

    Add files via upload

---
+++ lab7/lab7/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace GrammarValidator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCodeInput = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtCodeInput
+            // 
+            this.txtCodeInput.Location = new System.Drawing.Point(369, 50);
+            this.txtCodeInput.Name = "txtCodeInput";
+            this.txtCodeInput.Size = new System.Drawing.Size(100, 20);
+            this.txtCodeInput.TabIndex = 0;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(409, 140);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(35, 13);
+            this.lblResult.TabIndex = 1;
+            this.lblResult.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtCodeInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCodeInput;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ lab7/lab7/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace GrammarValidator
+{
+    public partial class Form1 : Form
+    {
+        // Patterns for grammar constructs
+        private const string StartSymbol = @"^(x|y)\s+(y|z)\s*;$"; // S -> A B ;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnValidate_Click(object sender, EventArgs e)
+        {
+            // Read input code from textbox
+            string code = txtCodeInput.Text;
+
+            // Validate the input code
+            if (IsValidGrammar(code))
+            {
+                lblResult.Text = "Valid grammar construct";
+                lblResult.ForeColor = System.Drawing.Color.Green;
+            }
+            else
+            {
+                lblResult.Text = "Invalid grammar construct";
+                lblResult.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+
+        // Method to check if the input matches the grammar rules
+        private bool IsValidGrammar(string code)
+        {
+            return MatchesPattern(StartSymbol, code);
+        }
+
+        // Helper method to match code with a given regex pattern
+        private bool MatchesPattern(string pattern, string code)
+        {
+            Regex regex = new Regex(pattern, RegexOptions.Singleline);
+            return regex.IsMatch(code);
+        }
+    }
+}
+
